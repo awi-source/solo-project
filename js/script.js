5651843aa4487f0a93deae8c141ff78b9e7fb7a8
Agnieszka Witek <witek.aga@gmail.com>
2021-02-27 18:42:06 +0100

    Add SCSS nesting

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 function toggleMenu() {
-    document.querySelector('.side-menu').classList.toggle('visible');
+    document.querySelector('.side-menu').classList.toggle('active');
   }
   document.querySelector('.fa-bars').addEventListener('click', function(e) {
     e.preventDefault();
